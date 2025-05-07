7d0d0118983d5be90928f2e68f7a83c631c67261
Alp Rüzgar <139581144+AlpRuzgar@users.noreply.github.com>
2025-05-07 16:46:16 +0300

    Delete recipe-manager directory

--- recipe-manager/src/main/java/com/example/recipemanager/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.recipemanager.repository;
-
-import com.example.recipemanager.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
